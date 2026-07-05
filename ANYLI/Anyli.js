aaf16998fdf9045cfa2fef927c9ff843b0ddfa32
Diana Fernandez <dfernandez2829@gmail.com>
2026-07-05 00:35:56 -0400

    Mejoras a la pagina
    Inclusion de un JavaScript y mejora de la seccion: catalogo

---
+++ ANYLI/Anyli.js
@@ -0,0 +1,6 @@
+
+window.alert("Al aceptar esta alerta, usted estara aceptando los terminos y condiciones de la pagina web ANYLI.");
+
+function mostrarAlerta() {
+    window.alert("Esto es una alerta desde el objeto window.");
+}
